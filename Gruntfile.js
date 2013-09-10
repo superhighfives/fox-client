d0ad0902e712f0ae5bbb65ddc0c13453be71e52f
Charlie Gleason <hi@superhighfives.com>
2013-09-10 14:17:31 +0100

    Updated the CNAME record and builded!

--- Gruntfile.js
+++ Gruntfile.js
@@ -293,7 +293,8 @@ module.exports = function (grunt) {
                         '*.{ico,png,txt}',
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
-                        'styles/fonts/{,*/}*.*'
+                        'styles/fonts/{,*/}*.*',
+                        'CNAME'
                     ]
                 }]
             },
